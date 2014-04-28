521068401d7552a18e510f3ba8a8417001d11741
masterbate <leftylemonzilla@gmail.com>
2014-04-29 03:30:19 +0430

    Made the VP harder

--- toontown/suit/DistributedSellbotBossAI.py
+++ toontown/suit/DistributedSellbotBossAI.py
@@ -189,9 +189,11 @@ class DistributedSellbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
 
     def generateSuits(self, battleNumber):
         if battleNumber == 1:
-            return self.invokeSuitPlanner(9, 0)
+			l = [0, 1]
+			skelOrNo = random.choice(l)
+            return self.invokeSuitPlanner(12, skelOrNo)
         else:
-            return self.invokeSuitPlanner(10, 1)
+            return self.invokeSuitPlanner(12, 1)
 
     def removeToon(self, avId):
         toon = simbase.air.doId2do.get(avId)
