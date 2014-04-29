11b1c1c48b9666723a7cd04504d210088ec34f7e
xPythonic <gr33nmyth@aol.com>
2014-04-29 17:37:57 -0400

    Remove "vpelevator" debug statement

--- toontown/building/DistributedVPElevatorAI.py
+++ toontown/building/DistributedVPElevatorAI.py
@@ -4,7 +4,6 @@ import DistributedBossElevatorAI
 class DistributedVPElevatorAI(DistributedBossElevatorAI.DistributedBossElevatorAI):
 
     def __init__(self, air, bldg, zone, antiShuffle = 0, minLaff = 0):
-        print "vpelevator"
         DistributedBossElevatorAI.DistributedBossElevatorAI.__init__(self, air, bldg, zone, antiShuffle=antiShuffle, minLaff=minLaff)
         self.type = ELEVATOR_VP
         self.countdownTime = ElevatorData[self.type]['countdown']
