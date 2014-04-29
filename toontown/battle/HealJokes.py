d1db57497f81534c1753aec88e4288c044ea09b2
Relltrem <RelltremGames@gmail.com>
2014-04-29 17:05:25 -0400

    Revert "Start of Heal Jokes"
    This reverts commit f2d63cfb53dc186a9c2605e609ee5af1f506552b.

--- toontown/battle/HealJokes.py
+++ toontown/battle/HealJokes.py
@@ -1,16 +1,2 @@
 from toontown.toonbase import TTLocalizer
 toonHealJokes = TTLocalizer.ToonHealJokes
-
-class HealJokes(StateData.StateData):
-    notify = DirectNotifyGlobal.directNotify.newCategory('HealJokes')
-
-    def __init__(self, doneEvent):
-        self.notify.debug('What goes TICK-TICK-TICK-WOOF? A watchdog!')
-        self.notify.debug('A watchdog!')
-        StateData.StateData.__init__(self, doneEvent)
-        self.numAvatars = 0
-        self.chosenAvatar = 0
-        self.toon = 1
-        self.loaded = 1
-
-        return None
