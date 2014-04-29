5321e567e8bf67606e9a85cad17686eb2bc289a1
xPythonic <gr33nmyth@aol.com>
2014-04-28 20:06:50 -0400

    Remove conflicting import
    This import caused an error on the VPS

--- toontown/coghq/DistributedStageBattleAI.py
+++ toontown/coghq/DistributedStageBattleAI.py
@@ -2,7 +2,7 @@ import CogDisguiseGlobals
 from direct.directnotify import DirectNotifyGlobal
 from direct.fsm import ClassicFSM, State
 from direct.fsm import State
-from direct.showbase.PythonUtil import addListsByValue, enumerate
+from direct.showbase.PythonUtil import addListsByValue
 from toontown.battle.BattleBase import *
 from toontown.coghq import DistributedLevelBattleAI
 from toontown.toonbase import ToontownGlobals
