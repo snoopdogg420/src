e674277d929716fbe054fb32a7eca23b3d4bb134
Chandler14362 <chandler14362@gmail.com>
2014-05-03 19:34:49 -0500

    New loading screen

--- toontown/toontowngui/ToontownLoadingScreen.py
+++ toontown/toontowngui/ToontownLoadingScreen.py
@@ -9,39 +9,45 @@ class ToontownLoadingScreen:
         self.__expectedCount = 0
         self.__count = 0
         self.gui = loader.loadModel('phase_3/models/gui/progress-background.bam')
-        self.banner = loader.loadModel('phase_3/models/gui/toon_council.bam').find('**/scroll')
-        self.banner.reparentTo(self.gui)
-        self.banner.setScale(0.4, 0.4, 0.4)
-        self.tip = DirectLabel(guiId='ToontownLoadingScreenTip', parent=self.banner, relief=None, text='', text_scale=TTLocalizer.TLStip, textMayChange=1, pos=(-1.2, 0.0, 0.1), text_fg=(0.4, 0.3, 0.2, 1), text_wordwrap=13, text_align=TextNode.ALeft)
-        self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(base.a2dRight/5, 0, 0.235), text='', textMayChange=1, text_scale=0.08, text_fg=(0, 0, 0.5, 1), text_align=TextNode.ALeft)
-        self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(base.a2dLeft+(base.a2dRight/4.95), base.a2dRight-(base.a2dRight/4.95), -0.03, 0.03), pos=(0, 0, 0.15), text='')
-
+        self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(base.a2dRight/5, 0, 0.235), text='', textMayChange=1, text_scale=0.08, text_fg=(0.03, 0.83, 0, 1), text_align=TextNode.ALeft, text_font=ToontownGlobals.getSignFont())
+        self.waitBar = self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(base.a2dLeft+(base.a2dRight/4.95), base.a2dRight-(base.a2dRight/4.95), -0.03, 0.03), pos=(0, 0, 0.15), text='')
+        self.toon = None
+        
     def destroy(self):
-        self.tip.destroy()
         self.title.destroy()
-        self.waitBar.destroy()
-        self.banner.removeNode()
         self.gui.removeNode()
-
+        if self.toon:
+            self.toon.delete()
+        
     def getTip(self, tipCategory):
         return TTLocalizer.TipTitle + '\n' + random.choice(TTLocalizer.TipDict.get(tipCategory))
 
     def begin(self, range, label, gui, tipCategory):
         self.waitBar['range'] = range
         self.title['text'] = label
-        self.tip['text'] = self.getTip(tipCategory)
         self.__count = 0
         self.__expectedCount = range
         if gui:
-            self.waitBar.reparentTo(self.gui)
-            self.waitBar.setPos(0, 0, 0.15)
-            self.title.reparentTo(self.gui)
-            self.gui.reparentTo(aspect2dp, NO_FADE_SORT_INDEX)
+            if base.localAvatarStyle:
+                from toontown.toon import Toon
+                self.toon = Toon.Toon()
+                self.toon.setDNA(base.localAvatarStyle)
+                self.toon.loop('bored', fromFrame=135, toFrame=135)
+                self.toon.getGeomNode().setDepthWrite(1)
+                self.toon.getGeomNode().setDepthTest(1)
+                self.toon.setHpr(205, 0, 0)
+                self.toon.setScale(0.18)
+                self.toon.setPos(base.a2dBottomRight.getX()/1.25, 0, -0.034)
+                self.toon.reparentTo(self.waitBar)
+                self.waitBar['frameSize'] = (base.a2dLeft+(base.a2dRight/8.15), base.a2dRight-(base.a2dRight/2.57), -0.03, 0.03)
+            self.title.reparentTo(base.a2dpBottomLeft, NO_FADE_SORT_INDEX)
+            self.title.setPos(0.24, 0, 0.23)
+            self.gui.setPos(0, -0.1, 0)
+            self.gui.reparentTo(aspect2d, NO_FADE_SORT_INDEX)
         else:
-            self.waitBar.reparentTo(base.a2dpBottomCenter, NO_FADE_SORT_INDEX)
-            self.waitBar.setPos(0, 0, 0.15)
             self.title.reparentTo(base.a2dpBottomLeft, NO_FADE_SORT_INDEX)
             self.gui.reparentTo(hidden)
+        self.waitBar.reparentTo(base.a2dpBottomCenter, NO_FADE_SORT_INDEX)
         self.waitBar.update(self.__count)
 
     def end(self):
@@ -49,6 +55,8 @@ class ToontownLoadingScreen:
         self.waitBar.reparentTo(self.gui)
         self.title.reparentTo(self.gui)
         self.gui.reparentTo(hidden)
+        if self.toon:
+            self.toon.reparentTo(hidden)
         return (self.__expectedCount, self.__count)
 
     def abort(self):
