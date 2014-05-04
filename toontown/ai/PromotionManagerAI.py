80c8f9dbe0d14bebdda1b3ffb3e53168eba8a4ca
xPythonic <gr33nmyth@aol.com>
2014-05-04 14:30:20 -0400

    Fix merits bug
    You can no longer collect merits for a Cog suit that you don't have.

--- toontown/ai/PromotionManagerAI.py
+++ toontown/ai/PromotionManagerAI.py
@@ -1,3 +1,4 @@
+from toontown.coghq import CogDisguiseGlobals
 from toontown.suit import SuitDNA
 
 
@@ -7,8 +8,16 @@ class PromotionManagerAI:
 
     def recoverMerits(self, toon, suitsKilled, zoneId, multiplier):
         cogMerits = toon.getCogMerits()
+        parts = toon.getCogParts()
+        completedSuits = (
+            CogDisguiseGlobals.isSuitComplete(parts, 0),
+            CogDisguiseGlobals.isSuitComplete(parts, 1),
+            CogDisguiseGlobals.isSuitComplete(parts, 2),
+            CogDisguiseGlobals.isSuitComplete(parts, 3)
+        )
         for suit in suitsKilled:
             deptIndex = SuitDNA.suitDepts.index(suit['track'])
-            cogMerits[deptIndex] += (suit['level']/2) * multiplier
+            if completedSuits[deptIndex]:
+                cogMerits[deptIndex] += (suit['level']/2) * multiplier
         toon.b_setCogMerits(cogMerits)
         return cogMerits
