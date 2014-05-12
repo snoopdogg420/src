054fc132a52b616d956e5cf44326535fc08a992d
xPythonic <gr33nmyth@aol.com>
2014-05-11 20:35:39 -0400

    Add prepare_client.py to the tools directory

---
+++ tools/prepare_client.py
@@ -0,0 +1,109 @@
+import argparse
+import os
+import subprocess
+import shutil
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--distribution', default='en', help='The distribution token.')
+parser.add_argument('--build-dir', default='build', help='The directory to store the build files in.')
+parser.add_argument('--src-dir', default='..', help='The directory of the Toontown Infinite source code.')
+parser.add_argument('--server-ver', default='tti-REVISION',
+                    help='An override for the server version of this build.\n'
+                         'Any REVISION tokens will be replaced with the current git revision.')
+parser.add_argument('--build-mfs', action='store_true', help='When present, multifiles will be built.')
+parser.add_argument('--resources-dir', default='../resources', help='The directory to the Toontown Infinite resources.')
+parser.add_argument('--main-module', default='infinite.base.ClientStartDist',
+                    help='The module that will be used to start the game.')
+parser.add_argument('modules', nargs='*', default=['shared', 'infinite'],
+                    help='The Toontown Infinite modules to be included in the build.')
+args = parser.parse_args()
+
+print 'Preparing the client...'
+
+# Create a clean build directory for us to store our build material:
+if os.path.exists(args.build_dir):
+    shutil.rmtree(args.build_dir)
+os.mkdir(args.build_dir)
+print 'Build directory = {0}'.format(args.build_dir)
+
+# This next part is only required if the user wants the Git revision in
+# their server version:
+revision = ''
+if 'REVISION' in args.server_ver:
+    # If we don't have Git on our path, let's attempt to add it:
+    paths = (
+        '{0}\\Git\\bin'.format(os.environ['ProgramFiles']),
+        '{0}\\Git\\cmd'.format(os.environ['ProgramFiles'])
+    )
+    for path in paths:
+        if path not in os.environ['PATH']:
+            os.environ['PATH'] += ';' + path
+
+    # Now, let's get that revision string:
+    revision = subprocess.Popen(
+        ['git', 'rev-parse', 'HEAD'],
+        stdout=subprocess.PIPE,
+        cwd=args.src_dir).stdout.read().strip()[:7]
+
+# Parse the server version:
+serverVersion = args.server_ver.replace('REVISION', revision)
+print 'serverVersion = {0}'.format(serverVersion)
+
+# Collect the DC filepaths:
+dcFiles = []
+dcFilePath = args.src_dir + '\\astron'
+for filename in os.listdir(dcFilePath):
+    if filename.endswith('.dc'):
+        dcFiles.append(os.path.join(dcFilePath, filename))
+print 'dcFiles = {0}'.format(dcFiles)
+
+# Copy the DC files:
+for dcFile in dcFiles:
+    outputFile = os.path.join(args.build_dir, dcFile.split('\\')[-1])
+    shutil.copyfile(dcFile, outputFile)
+
+# Copy the required module files:
+excludes = ('NonRepeatableRandomSourceUD.py', 'NonRepeatableRandomSourceAI.py')
+for module in args.modules:
+    print 'Writing module...', module
+    for root, folders, files in os.walk(os.path.join(args.src_dir, module)):
+        outputDir = root.replace(args.src_dir, args.build_dir)
+        if not os.path.exists(outputDir):
+            os.mkdir(outputDir)
+        for filename in files:
+            if not filename.endswith('.py'):
+                continue
+            if filename.endswith('UD.py') and (filename not in excludes):
+                continue
+            if filename.endswith('AI.py') and (filename not in excludes):
+                continue
+            shutil.copyfile(os.path.join(root, filename), os.path.join(outputDir, filename))
+
+# Copy the config file with the replaced SERVER_VERSION token:
+configFileName = 'config_{0}.prc'.format(args.distribution)
+print 'Copying config...', configFileName
+f = open(os.path.join(args.src_dir, 'config', configFileName), 'r')
+data = f.read()
+f.close()
+out = open(os.path.join(args.build_dir, configFileName), 'w')
+out.write(data.replace('SERVER_VERSION', serverVersion))
+out.close()
+
+# Build the multifiles if wanted:
+if args.build_mfs:
+    print 'Building multifiles...'
+    dest = os.path.join(args.build_dir, 'resources')
+    os.mkdir(dest)
+    dest = os.path.realpath(dest)
+    os.chdir(args.resources_dir)
+    for phase in os.listdir('.'):
+        if not phase.startswith('phase_'):
+            continue
+        if not os.path.isdir(phase):
+            continue
+        out = os.path.join(dest, phase + '.mf')
+        print 'Writing...', out
+        os.system('multify -c -f {0} {1}'.format(out, phase))
+else:
+    print 'Skipping multifiles...'
