4dc18ca9e444f6d0822f386725ef986422e4f73e
xPythonic <gr33nmyth@aol.com>
2014-05-21 07:42:19 -0400

    Fix typo in last commit

--- toontown/minigame/PatternGameGlobals.py
+++ toontown/minigame/PatternGameGlobals.py
@@ -1,7 +1,7 @@
 import MinigameGlobals
 
 
-if base.config.GetString('want-long-pattern-game', False):
+if base.config.GetBool('want-long-pattern-game', False):
     INITIAL_ROUND_LENGTH = 1
     ROUND_LENGTH_INCREMENT = 1
     NUM_ROUNDS = 100
