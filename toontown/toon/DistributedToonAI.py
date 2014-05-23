5ef6d0010443b910e985acd83b6eb244b5c9ce5e
xPythonic <gr33nmyth@aol.com>
2014-05-23 05:36:35 -0400

    Remove 'Yes', 'No', and 'OK' from the ~unlocks command's emotion reward

--- toontown/toon/DistributedToonAI.py
+++ toontown/toon/DistributedToonAI.py
@@ -4618,6 +4618,10 @@ def unlocks():
     for emoteId in OTPLocalizer.EmoteFuncDict.values():
         if emoteId >= len(emotes):
             continue
+        # The following emotions are ignored because they are unable to be
+        # obtained:
+        if emoteId in (17, 18, 19):
+            continue
         emotes[emoteId] = 1
     invoker.b_setEmoteAccess(emotes)
 
