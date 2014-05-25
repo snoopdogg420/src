8e93b85fc14a89209adead84999c387d58e1ff0d
xPythonic <gr33nmyth@aol.com>
2014-05-25 05:06:37 -0400

    Fix Cog HQ crash

--- toontown/coghq/FactoryExterior.py
+++ toontown/coghq/FactoryExterior.py
@@ -69,16 +69,20 @@ class FactoryExterior(BattlePlace.BattlePlace):
         loadDNAFileAI(dnaStore, dnaFileName)
 
         # Collect all of the vis group zone IDs:
-        self.zoneVisList = []
+        self.zoneVisDict = []
         for i in xrange(dnaStore.getNumDNAVisGroupsAI()):
             groupFullName = dnaStore.getDNAVisGroupName(i)
             visGroup = dnaStore.getDNAVisGroupAI(i)
             visZoneId = int(base.cr.hoodMgr.extractGroupName(groupFullName))
             visZoneId = ZoneUtil.getTrueZoneId(visZoneId, self.zoneId)
-            self.zoneVisList.append(visZoneId)
+            visibles = []
+            for i in xrange(visGroup.getNumVisibles()):
+                visibles.append(int(visGroup.visibles[i]))
+            visibles.append(ZoneUtil.getBranchZone(visZoneId))
+            self.zoneVisDict[visZoneId] = visibles
 
         # Next, we want interest in all vis groups due to this being a Cog HQ:
-        base.cr.sendSetZoneMsg(self.zoneId, self.zoneVisList)
+        base.cr.sendSetZoneMsg(self.zoneId, self.zoneVisDict.values()[0])
 
         BattlePlace.BattlePlace.enter(self)
         self.fsm.enterInitialState()
--- toontown/coghq/SellbotHQExterior.py
+++ toontown/coghq/SellbotHQExterior.py
@@ -18,16 +18,20 @@ class SellbotHQExterior(CogHQExterior.CogHQExterior):
         loadDNAFileAI(dnaStore, dnaFileName)
 
         # Collect all of the vis group zone IDs:
-        self.zoneVisList = []
+        self.zoneVisDict = []
         for i in xrange(dnaStore.getNumDNAVisGroupsAI()):
             groupFullName = dnaStore.getDNAVisGroupName(i)
             visGroup = dnaStore.getDNAVisGroupAI(i)
             visZoneId = int(base.cr.hoodMgr.extractGroupName(groupFullName))
             visZoneId = ZoneUtil.getTrueZoneId(visZoneId, self.zoneId)
-            self.zoneVisList.append(visZoneId)
+            visibles = []
+            for i in xrange(visGroup.getNumVisibles()):
+                visibles.append(int(visGroup.visibles[i]))
+            visibles.append(ZoneUtil.getBranchZone(visZoneId))
+            self.zoneVisDict[visZoneId] = visibles
 
         # Next, we want interest in all vis groups due to this being a Cog HQ:
-        base.cr.sendSetZoneMsg(self.zoneId, self.zoneVisList)
+        base.cr.sendSetZoneMsg(self.zoneId, self.zoneVisDict.values()[0])
 
     def exit(self):
         self.loader.hood.stopSky()
