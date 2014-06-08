1a4e3a41af20bf5f3e4383da449588892fd3f187
masterbate <leftylemonzilla@gmail.com>
2014-06-08 19:27:57 +0200

    Fixed AI server crash when using ~~kick on a cog

--- otp/ai/MagicWordManagerAI.py
+++ otp/ai/MagicWordManagerAI.py
@@ -9,8 +9,12 @@ class MagicWordManagerAI(DistributedObjectAI):
 
     def sendMagicWord(self, word, targetId):
         invokerId = self.air.getAvatarIdFromSender()
-
         invoker = self.air.doId2do.get(invokerId)
+
+        if not 'DistributedToonAI' in str(self.air.doId2do.get(targetId)):
+            self.sendUpdateToAvatarId(invokerId, 'sendMagicWordResponse', ['Target is not a toon object!'])
+            return
+            
         if not invoker:
             self.sendUpdateToAvatarId(invokerId, 'sendMagicWordResponse', ['missing invoker'])
             return
