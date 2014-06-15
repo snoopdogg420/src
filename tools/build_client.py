897994c3d64a131b6c1a089867fffed8088e529d
xPythonic <gr33nmyth@aol.com>
2014-06-15 11:16:50 -0400

    Tools: build_client.py: Remove deprecated --build-secret option

--- tools/build_client.py
+++ tools/build_client.py
@@ -7,8 +7,6 @@ parser.add_argument('--panda3d-dir', default='C:/Panda3D-1.9.0',
                     help='The path to the Panda3D build to use for this distribution.')
 parser.add_argument('--main-module', default='infinite.base.ClientStartDist',
                     help='The path to the instantiation module.')
-parser.add_argument('--build-secret', default='dev',
-                    help='The secret that will be used to encrypt the build.')
 parser.add_argument('modules', nargs='*', default=['shared', 'infinite'],
                     help='The Toontown Infinite modules to be included in the build.')
 args = parser.parse_args()
