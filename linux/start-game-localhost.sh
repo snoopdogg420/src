201677e7969d1997d817a16f34997a184e0f3d78
xPythonic <gr33nmyth@aol.com>
2014-06-28 16:53:56 -0400

    Linux: Remove usage of PPYTHON_PATH
    This is unnecessary.

--- linux/start-game-localhost.sh
+++ linux/start-game-localhost.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 cd ..
 
-# Read the contents of PPYTHON_PATH into $PPYTHON_PATH:
-PPYTHON_PATH=`cat PPYTHON_PATH`
-
 # Get the user input:
 read -p "Username: " ttiUsername
 
@@ -15,9 +12,8 @@ export TTI_GAMESERVER="127.0.0.1"
 
 echo "==============================="
 echo "Starting Toontown Infinite..."
-echo "ppython: $PPYTHON_PATH"
 echo "Username: $ttiUsername"
 echo "Gameserver: $TTI_GAMESERVER"
 echo "==============================="
 
-$PPYTHON_PATH -m toontown.toonbase.ClientStart
+/usr/bin/python2 -m toontown.toonbase.ClientStart
