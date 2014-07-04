aac54d765ff22e47df6f3323e949c9aa99b3d2eb
xPythonic <gr33nmyth@aol.com>
2014-07-03 22:31:21 -0400

    Estate: Fix phone movies

--- toontown/estate/DistributedPhoneAI.py
+++ toontown/estate/DistributedPhoneAI.py
@@ -5,7 +5,7 @@ from PhoneGlobals import *
 
 class DistributedPhoneAI(DistributedFurnitureItemAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedPhoneAI")
-    
+
     def __init__(self, air, furnitureMgr, catalogItem, ownerId):
         DistributedFurnitureItemAI.__init__(self, air, furnitureMgr, catalogItem)
         self.initialScale = (0.8, 0.8, 0.8)
@@ -16,22 +16,21 @@ class DistributedPhoneAI(DistributedFurnitureItemAI):
     def setInitialScale(self, sx, sy, sz):
         self.initialScale = (sx, sy, sz)
         self.sendUpdate('setInitialScale', args=[sx, sy, sz])
-    
+
     def getInitialScale(self):
         return self.initialScale
 
     def setNewScale(self, sx, sy, sz):
         self.newScale = (sx, sy, sz)
         self.sendUpdate('setNewScale', args=[sx, sy, sz])
-        
+
     def getNewScale(self):
         return self.newScale
 
     def avatarEnter(self):
         avId = self.air.getAvatarIdFromSender()
-        
+
         if not self.busy:
-            print 'av enter phone %s'%(avId)
             self.sendEnterMovie(avId)
             self.air.questManager.toonUsedPhone(avId)
             self.busy = avId
@@ -39,10 +38,9 @@ class DistributedPhoneAI(DistributedFurnitureItemAI):
             self.freeAvatar(avId)
 
     def avatarExit(self):
-        print 'av exit phone %s'%(self.busy)
         self.sendExitMovie()
         self.sendClearMovie()
-        
+
         self.busy = 0
 
     def freeAvatar(self, avId):
@@ -52,15 +50,15 @@ class DistributedPhoneAI(DistributedFurnitureItemAI):
         pass
 
     def setMovie(self, mode, avId):
-        self.sendUpdate('setMovie', args=[mode, avId,
-                            ClockDelta.globalClockDelta.getRealNetworkTime()])
+        timestamp = ClockDelta.globalClockDelta.getRealNetworkTime(bits=32)
+        self.sendUpdate('setMovie', args=[mode, avId, timestamp])
 
     def sendEnterMovie(self, avId):
-        self.setMovie(PHONE_MOVIE_PICKUP, avId)        
-    
+        self.setMovie(PHONE_MOVIE_PICKUP, avId)
+
     def sendExitMovie(self):
         self.setMovie(PHONE_MOVIE_HANGUP, self.busy)
-        
+
     def sendClearMovie(self):
         self.setMovie(PHONE_MOVIE_CLEAR, self.busy)
 
@@ -75,4 +73,3 @@ class DistributedPhoneAI(DistributedFurnitureItemAI):
 
     def requestGiftPurchaseResponse(self, context, retcode):
         self.sendUpdate('requestGiftPurchaseResponse', args=[context, retcode])
-
