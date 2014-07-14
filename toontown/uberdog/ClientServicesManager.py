cec5b82c56d09540992ef75dffc8ba095c8acc8b
masterbate <leftylemonzilla@gmail.com>
2014-07-14 12:13:04 +0200

    Implement HMAC
    Anti-Hack

--- toontown/uberdog/ClientServicesManager.py
+++ toontown/uberdog/ClientServicesManager.py
@@ -2,6 +2,7 @@ from direct.distributed.DistributedObjectGlobal import DistributedObjectGlobal
 from direct.directnotify.DirectNotifyGlobal import directNotify
 from otp.distributed.PotentialAvatar import PotentialAvatar
 from pandac.PandaModules import *
+import hmac
 
 class ClientServicesManager(DistributedObjectGlobal):
     notify = directNotify.newCategory('ClientServicesManager')
@@ -9,11 +10,16 @@ class ClientServicesManager(DistributedObjectGlobal):
     # --- LOGIN LOGIC ---
     def performLogin(self, doneEvent):
         self.doneEvent = doneEvent
+        key = 'cGFzY2FsYW5kY29keWFyZXNvZnVja2luZ2hvdGl0aXNpbnNhbmU='
 
         token = self.cr.playToken or 'dev'
 
         self.notify.debug('Sending token: ' + token)
-        self.sendUpdate('login', [token])
+        digest_maker = hmac.new(key)
+        digest_maker.update(token)
+        clientKey = digest_maker = hexdigest()
+        
+        self.sendUpdate('login', [token, clientKey])
 
     def acceptLogin(self, timestamp):
         messenger.send(self.doneEvent, [{'mode': 'success', 'timestamp': timestamp}])
