49e31211ee077d89c4e81243aa766f6ed31aafdf
masterbate <leftylemonzilla@gmail.com>
2014-07-18 01:57:09 +0200

    DC

--- otp/distributed/DCClassImports.py
+++ otp/distributed/DCClassImports.py
@@ -2,7 +2,7 @@
 from pandac.PandaModules import *
 
 
-hashVal = 638088394
+hashVal = 3014038059L
 
 
 from toontown.cogdominium import DistributedCogdoInterior, DistributedCogdoBattleBldg, DistributedCogdoElevatorExt, DistributedCogdoElevatorInt, DistributedCogdoBarrel, DistCogdoGame, DistCogdoLevelGame, DistCogdoBoardroomGame, DistCogdoCraneGame, DistCogdoMazeGame, DistCogdoFlyingGame, DistCogdoCrane, DistCogdoCraneMoneyBag, DistCogdoCraneCog
