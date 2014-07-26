40c6aca14c8bfaa40d73732e431f70b2f7c0783f
Peter Mango <lifelesspeter101@aol.com>
2014-07-26 14:16:57 -0400

    Friend manager fix
    Thanks bryan >_<

--- toontown/friends/TTIFriendsManagerUD.py
+++ toontown/friends/TTIFriendsManagerUD.py
@@ -86,8 +86,9 @@ class TTIFriendsManagerUD(DistributedObjectGlobalUD):
             if not (avId or friendId):
                 return
             if dclass == self.air.dclassesByName['DistributedToonUD']:
-                if avId in self.listResponses:
-                    self.listResponses[avId].append([friendId, fields['setName'][0], fields['setDNAString'][0], fields['setPetId'][0]])
+                if avId not in self.listResponses:
+                    self.listResponses[avId] = []
+                self.listResponses[avId].append([friendId, fields['setName'][0], fields['setDNAString'][0], fields['setPetId'][0]])
             if len(self.listResponses[avId]) >= len(self.friendsLists[avId]):
                 self.sendUpdateToAvatarId(avId, 'friendList', [self.listResponses[avId]])
                 del self.friendsLists[avId]
