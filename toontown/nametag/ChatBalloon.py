697d482b1f40819e011f44187a75af6b6be7cac6
xPythonic <gr33nmyth@aol.com>
2014-08-05 15:50:54 -0400

    Nametag: Add min width and height for chat balloons

--- toontown/nametag/ChatBalloon.py
+++ toontown/nametag/ChatBalloon.py
@@ -6,6 +6,8 @@ class ChatBalloon(NodePath):
     TEXT_Z_OFFSET = -0.1
     CHAT_BALLOON_X_PADDING = 0.5
     CHAT_BALLOON_Z_PADDING = 0.5
+    CHAT_BUBBLE_MIN_WIDTH = 2.0
+    CHAT_BUBBLE_MIN_HEIGHT = 2.0
 
     def __init__(self, model, modelWidth, modelHeight, chatTextNode,
                  foreground=VBase4(0, 0, 0, 1), background=VBase4(1, 1, 1, 1)):
@@ -30,8 +32,12 @@ class ChatBalloon(NodePath):
         chatTextWidth = chatTextNode.getWidth()
         chatTextHeight = chatTextNode.getHeight()
         paddedWidth = chatTextWidth + (ChatBalloon.CHAT_BALLOON_X_PADDING*2)
+        if paddedWidth < ChatBalloon.CHAT_BALLOON_MIN_WIDTH:
+            paddedWidth = ChatBalloon.CHAT_BALLOON_MIN_WIDTH
         chatBalloon.setSx(paddedWidth / modelWidth)
         paddedHeight = chatTextHeight + (ChatBalloon.CHAT_BALLOON_Z_PADDING*2)
+        if paddedHeight < ChatBalloon.CHAT_BALLOON_MIN_HEIGHT:
+            paddedHeight = ChatBalloon.CHAT_BALLOON_MIN_HEIGHT
         middle.setSz(paddedHeight - 1.5)  # Compensate for the top, as well.
         top.setZ(middle, 1)
 
