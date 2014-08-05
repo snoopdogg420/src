44bcdb7a940cab66f9a9f40613cf2ad638addb11
xPythonic <gr33nmyth@aol.com>
2014-08-05 15:26:59 -0400

    Nametag: Fix chat bubble top compensation

--- toontown/nametag/ChatBalloon.py
+++ toontown/nametag/ChatBalloon.py
@@ -3,8 +3,8 @@ from pandac.PandaModules import *
 
 class ChatBalloon(NodePath):
     TEXT_Y_OFFSET = -0.05
-    CHAT_BUBBLE_X_PADDING = 0.5
-    CHAT_BUBBLE_Z_PADDING = 0.5
+    CHAT_BALLOON_X_PADDING = 0.5
+    CHAT_BALLOON_Z_PADDING = 0.5
 
     def __init__(self, model, modelWidth, modelHeight, chatTextNode,
                  foreground=VBase4(0, 0, 0, 1), background=VBase4(1, 1, 1, 1)):
@@ -28,15 +28,17 @@ class ChatBalloon(NodePath):
         top = chatBalloon.find('**/top')
         chatTextWidth = chatTextNode.getWidth()
         chatTextHeight = chatTextNode.getHeight()
-        paddedWidth = chatTextWidth + (ChatBalloon.CHAT_BUBBLE_X_PADDING*2)
+        paddedWidth = chatTextWidth + (ChatBalloon.CHAT_BALLOON_X_PADDING*2)
         chatBalloon.setSx(paddedWidth / modelWidth)
-        paddedHeight = chatTextHeight + (ChatBalloon.CHAT_BUBBLE_Z_PADDING*2)
-        middle.setSz(paddedHeight)
+        paddedHeight = chatTextHeight + (ChatBalloon.CHAT_BALLOON_Z_PADDING*2)
+        middle.setSz(paddedHeight - 1.5)  # Compensate for the top, as well.
+        top.setZ(middle, 1)
 
         # Position the TextNode:
         chatText.setPos(chatBalloon.getBounds().getCenter())
         chatText.setY(ChatBalloon.TEXT_Y_OFFSET)
         chatText.setX(chatText, -(chatTextWidth/2))
+        chatText.setZ(top, -ChatBalloon.CHAT_BALLOON_Z_PADDING)
 
         # Compensate for the top:
         middle.setSz(paddedHeight - 1.25)
