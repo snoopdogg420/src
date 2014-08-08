1c831e5f11f754846a9b64071d04eab5d20e3208
Chan <chandler14362@gmail.com>
2014-08-08 12:38:37 -0400

    DNAParser: Renamed storage to dnaStorage in DNAParser

--- toontown/dna/DNAParser.py
+++ toontown/dna/DNAParser.py
@@ -1,22 +1,22 @@
 import DNALoader
 from DNAStorage import DNAStorage
 
-def loadDNAFile(storage, file):
+def loadDNAFile(dnaStorage, file):
     print 'Reading DNA file...', file
     dnaLoader = DNALoader.DNALoader()
     if __debug__:
         file = '../resources/' + file
     else:
         file = '/' + file
-    return dnaLoader.loadDNAFile(storage, file)
+    return dnaLoader.loadDNAFile(dnaStorage, file)
 
-def loadDNAFileAI(storage, file):
+def loadDNAFileAI(dnaStorage, file):
     dnaLoader = DNALoader.DNALoader()
     if __debug__:
         file = '../resources/' + file
     else:
         file = '/' + file
-    return dnaLoader.loadDNAFileAI(storage, file)
+    return dnaLoader.loadDNAFileAI(dnaStorage, file)
 
-def loadDNABulk(storage, files):
+def loadDNABulk(dnaStorage, files):
     pass # TODO
