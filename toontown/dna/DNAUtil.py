ddc561f5d0e2d8105ca2db2383d59e868e87eacb
Chan <chandler14362@gmail.com>
2014-08-08 16:45:01 -0400

    DNAParser: Fixed DNAUtil

--- toontown/dna/DNAUtil.py
+++ toontown/dna/DNAUtil.py
@@ -1,8 +1,8 @@
-from pandac.PandaModules import *
+from panda3d.core import LVector4f
 
 def dgiExtractString8(dgi):
     return dgi.extractBytes(dgi.getUint8())
 
 def dgiExtractColor(dgi):
-    return tuple(dgi.getUint8() / 255. for _ in xrange(4))
+    return LVector4f(dgi.getUint8() / 255. for _ in xrange(4))
 
