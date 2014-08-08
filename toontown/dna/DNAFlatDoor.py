fa9e904481d9dd3dbbdbda193e695c24ff1c85ae
Chan <chandler14362@gmail.com>
2014-08-08 15:01:40 -0400

    DNAParser: Add DNAFlatDoor

---
+++ toontown/dna/DNAFlatDoor.py
@@ -0,0 +1,13 @@
+import DNADoor
+from DNAUtil import *
+
+class DNAFlatDoor(DNADoor.DNADoor):
+    COMPONENT_CODE = 18
+
+    def traverse(self, nodePath, dnaStorage):
+        node = dnaStorage.findNode(self.getCode())
+        node = node.copyTo(nodePath, 0)
+        node.setScale(NodePath(), (1, 1, 1))
+        node.setPosHpr((0.5, 0, 0), (0, 0, 0))
+        node.setColor(self.getColor())
+        node.getNode(0).setEffect(DecalEffect.make())
