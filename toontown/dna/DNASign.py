88bc328f9f3b855ecd55304ab5b9c5495bbc0258
Chan <chandler14362@gmail.com>
2014-08-08 13:36:28 -0400

    DNAParser: Add DNASign

---
+++ toontown/dna/DNASign.py
@@ -0,0 +1,48 @@
+import DNANode
+from DNAUtil import *
+
+class DNASign(DNANode.DNANode):
+    COMPONENT_CODE = 5
+
+    def __init__(self):
+        DNANode.DNANode.__init__(self, '')
+        self.code = ''
+        self.color = (1, 1, 1, 1)
+
+    def getCode(self):
+        return self.code
+
+    def setCode(self, code):
+        self.code = code
+
+    def getColor(self):
+        return self.color
+
+    def setColor(self, color):
+        self.color = color
+
+    def makeFromDGI(self, dgi):
+        DNANode.DNANode.makeFromDGI(self, dgi)
+        self.code = dgiExtractString8(dgi)
+        self.color = dgiExtractColor(dgi)
+
+    def traverse(self, nodePath, dnaStorage):
+        decNode = nodePath.find('**/sign_decal')
+        if decNode.isEmpty():
+            decNode = nodePath.find('**/*_front')
+        if decNode.isEmpty() or not decNode.getNode(0).isGeomNode():
+            decNode = nodePath.find('**/+GeomNode')
+        node = None
+        if self.code != '':
+            node = dnaStorage.findNode(self.code)
+            node = node.copyTo(decNode, 0)
+            node.setName('sign')
+        else:
+            node = ModelNode('sign')
+            node = decNode.attachNewNode(node, 0)
+        node.setDepthOffset(50)
+        origin = nodePath.find('**/*sign_origin')
+        node.setPosHprScale(origin, self.pos, self.hpr, self.scale)
+        for child in self.children:
+            child.traverse(node, dnaStorage)
+        node.flattenStrong()
