4e89c3b35bc7cb42557ea2341f48a4f40abcf882
Chan <chandler14362@gmail.com>
2014-08-08 13:51:28 -0400

    DNAParser: Add DNASignBaseline

---
+++ toontown/dna/DNASignBaseline.py
@@ -0,0 +1,30 @@
+import DNANode
+from DNAUtil import *
+
+class DNASignBaseline(DNANode.DNANode):
+    COMPONENT_CODE = 6
+
+    def __init__(self):
+        DNANode.DNANode.__init__(self, '')
+        self.data = ''
+
+    def makeFromDGI(self, dgi):
+        DNANode.DNANode.makeFromDGI(self, dgi)
+        self.data = dgi.getString()
+        if len(self.data):
+            self.data = decompressString(self.data)
+
+    def traverse(self, nodePath, dnaStorage):
+        node = nodePath.attachNewNode('sign-baseline')
+        node.setPos(self.pos)
+        node.setHpr(self.hpr)
+        node.setDepthOffset(50)
+        if self.data:
+            bf = BamFile()
+            ss = StringStream()
+            ss.setData(self.data)
+            bf.openRead(ss)
+            signText = NodePath(bf.readNode())
+            signText.reparentTo(node)
+        for child in self.children:
+            child.traverse(nodePath, dnaStorage)
