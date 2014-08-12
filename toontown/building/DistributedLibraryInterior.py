75566131219edb413bc393a80e47a0ccefb774f9
xPythonic <gr33nmyth@aol.com>
2014-08-12 14:53:11 -0400

    Building: Add random to library interior

--- toontown/building/DistributedLibraryInterior.py
+++ toontown/building/DistributedLibraryInterior.py
@@ -1,4 +1,5 @@
 from direct.distributed.DistributedObject import DistributedObject
+import random
 
 from toontown.building import  ToonInteriorColors
 from toontown.dna.DNAParser import DNADoor
@@ -12,6 +13,10 @@ class DistributedLibraryInterior(DistributedObject):
 
         self.interior = loader.loadModel('phase_4/models/modules/ttc_library_interior.egg')
         self.interior.reparentTo(render)
+
+        generator = random.Random()
+        generator.seed(self.zoneId)
+        self.replaceRandom(self.interior, generator=generator)
 
         doorOrigin = self.interior.find('**/door_origin;+s')
         doorOrigin.setScale(0.8)
@@ -33,7 +38,6 @@ class DistributedLibraryInterior(DistributedObject):
         for npcToon in self.cr.doFindAllInstances(DistributedNPCToonBase):
             npcToon.initToonState()
 
-
     def disable(self):
         self.interior.removeNode()
         del self.interior
@@ -43,3 +47,27 @@ class DistributedLibraryInterior(DistributedObject):
     def setZoneIdAndBlock(self, zoneId, block):
         self.zoneId = zoneId
         self.block = block
+
+    def replaceRandom(self, root, generator=random):
+        for nodePath in root.findAllMatches('**/random_???_*'):
+            name = nodePath.getName()
+
+            category = name[11:]
+
+            if name[7] in ('m', 't'):
+                codeCount = self.cr.playGame.dnaStore.getNumCatalogCodes(category)
+                index = generator.randint(0, codeCount - 1)
+                code = self.cr.playGame.dnaStore.getCatalogCode(category, index)
+                if name[7] == 'm':
+                    _nodePath = self.cr.playGame.dnaStore.findNode(code).copyTo(nodePath)
+                    if name[8] == 'r':
+                        self.replaceRandom(_nodePath, generator=generator)
+                else:
+                    texture = self.cr.playGame.dnaStore.findTexture(code)
+                    nodePath.setTexture(texture, 100)
+                    _nodePath = nodePath
+
+            if name[8] == 'c':
+                hoodId = ZoneUtil.getCanonicalHoodId(self.zoneId)
+                colors = ToonInteriorColors.colors[hoodId]
+                _nodePath.setColorScale(generator.choice(colors[category]))
