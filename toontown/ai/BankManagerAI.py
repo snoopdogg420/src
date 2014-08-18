d30db89553bd28d58577f6de41559318871cb124
joshsora <joshsoragaming@gmail.com>
2014-08-18 13:02:26 +0100

    AI: Fix an issue with the BankManager

--- toontown/ai/BankManagerAI.py
+++ toontown/ai/BankManagerAI.py
@@ -4,6 +4,7 @@ from toontown.toonbase import ToontownGlobals
 class OperationFSM(FSM):
 
     def __init__(self, air, bankMgr):
+        FSM.__init__(self, 'OperationFSM')
         self.air = air
         self.bankMgr = bankMgr
 
@@ -29,10 +30,10 @@ class BankRetrieveFSM(OperationFSM):
                 return
 
             av.b_setBankMoney(money)
-            self.demand('Off', money)
+            self.demand('Off')
 
-    def enterOff(self, result):
-        messenger.send('bankDone-%s' % self.target, [result])
+    def enterOff(self):
+        messenger.send('bankDone-%s' % self.target)
         OperationFSM.enterOff(self)
 
 class BankUpdateFSM(OperationFSM):
@@ -40,7 +41,7 @@ class BankUpdateFSM(OperationFSM):
     def enterStart(self, avId, DISLid, money):
         self.target = avId
 
-        money = min(money, ToontownGlobals.maxBankMoney)
+        money = min(money, ToontownGlobals.MaxBankMoney)
         if money < 0:
             self.demand('Off')
             return
@@ -67,7 +68,7 @@ class BankManagerAI:
 
     def performFSM(self, target, fsmClass, *args):
         self.avId2fsm[target] = fsmClass(self.air, self)
-        self.avId2fsm[target].demand('Start', *args)
+        self.avId2fsm[target].request('Start', *args)
 
     def setMoney(self, avId, money):
         av = self.air.doId2do.get(avId)
