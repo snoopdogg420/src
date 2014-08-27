f2e2b4aab907c87c656e4b82f451a8cd4b0e294b
xPythonic <gr33nmyth@aol.com>
2014-08-27 02:07:18 -0400

    Movie: Add more scalability to the MovieTalkEvent

--- toontown/movie/MovieTalkEvent.py
+++ toontown/movie/MovieTalkEvent.py
@@ -5,19 +5,22 @@ from otp.nametag.NametagConstants import CFSpeech
 
 
 class MovieTalkEvent(MovieEvent):
-    def __init__(self, track, avatar, message):
+    def __init__(self, track, avatar, message, chatFlags=CFSpeech, timePerWord=0.55):
         MovieEvent.__init__(self, track)
 
         self.avatar = avatar
         self.message = message
+        self.chatFlags = chatFlags
+        self.timePerWord = timePerWord
 
-        self.messageTime = 0.5 * len(self.message.split(' '))
+        self.duration = self.timePerWord * len(self.message.split(' '))
 
     def construct(self):
         MovieEvent.construct(self)
 
         track = Sequence()
-        track.append(Func(self.avatar.setChatAbsolute, self.message, CFSpeech))
-        track.append(Wait(self.messageTime))
+        track.append(Func(self.avatar.setChatAbsolute, self.message, self.chatFlags))
+        track.append(Wait(self.duration))
         track.append(Func(self.avatar.clearChat))
+
         return track
