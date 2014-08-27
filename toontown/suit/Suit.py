94f0e69ff9760dbba94a5703b06aa571c938032c
xPythonic <gr33nmyth@aol.com>
2014-08-26 20:43:17 -0400

    Suit Fix import error

--- toontown/suit/Suit.py
+++ toontown/suit/Suit.py
@@ -10,7 +10,7 @@ from toontown.battle import BattleProps
 from toontown.toonbase import TTLocalizer
 from pandac.PandaModules import VirtualFileMountHTTP, VirtualFileSystem, Filename, DSearchPath
 from direct.showbase import AppRunnerGlobal
-from otp.nametag import NametagGroup
+from toontown.nametag import NametagGroup
 import string
 import os
 from toontown.suit import SuitGlobals
