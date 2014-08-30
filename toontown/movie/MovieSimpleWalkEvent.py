d54f971137bc1175fdb09010753f8bc71991cd5a
xPythonic <gr33nmyth@aol.com>
2014-08-29 20:40:08 -0400

    Movie: Refactor MovieSimpleWalkEvent due to MovieSimpleRotateWalkEvent

--- toontown/movie/MovieSimpleWalkEvent.py
+++ toontown/movie/MovieSimpleWalkEvent.py
@@ -1,103 +1,52 @@
-from direct.interval.IntervalGlobal import LerpQuatInterval, LerpPosInterval
+from direct.interval.IntervalGlobal import LerpPosInterval
 from direct.interval.IntervalGlobal import Sequence, Parallel, Func
-from direct.interval.IntervalGlobal import SoundInterval
 
 from otp.movie.MovieEvent import MovieEvent
 from otp.otpbase import OTPGlobals
 
 
 class MovieSimpleWalkEvent(MovieEvent):
-    def __init__(self, avatar, quatA, quatB, pointA, pointB,
-                 rotateSpeed=OTPGlobals.ToonRotateSpeed,
-                 walkSpeed=OTPGlobals.ToonForwardSpeed,
-                 rotateAnimName='walk', rotateAnimPlayRate=1,
-                 walkAnimName='walk', walkAnimPlayRate=1, rotateSfx=None,
-                 walkSfx=None):
+    def __init__(self, avatar, pointA, pointB,
+                 speed=OTPGlobals.ToonForwardSpeed, animName='run',
+                 animPlayRate=1, sfx=None):
         self.avatar = avatar
-        self.quatA = quatA
-        self.quatB = quatB
         self.pointA = pointA
         self.pointB = pointB
-        self.rotateSpeed = rotateSpeed
-        self.walkSpeed = walkSpeed
-        self.rotateAnimName = rotateAnimName
-        self.rotateAnimPlayRate = rotateAnimPlayRate
-        self.walkAnimName = walkAnimName
-        self.walkAnimPlayRate = walkAnimPlayRate
-        self.rotateSfx = rotateSfx
-        self.walkSfx = walkSfx
+        self.speed = speed
+        self.animName = animName
+        self.animPlayRate = animPlayRate
+        self.sfx = sfx
 
     def construct(self):
         track = Sequence()
 
-        # First, set the avatar's positional components to its starting point:
+        # First, set the avatar's initial positional components:
         track.append(Func(self.avatar.setPos, self.pointA))
-        track.append(Func(self.avatar.setQuat, self.quatA))
 
-        # Next, construct the rotate track:
-        rotateTrack = Parallel()
-        if self.rotateAnimName is not None:
-            rotateTrack.append(
-                Func(self.avatar.setPlayRate, self.rotateAnimPlayRate,
-                     self.rotateAnimName)
-            )
-            rotateTrack.append(
-                Func(self.avatar.loop, self.rotateAnimName)
-            )
-        if self.rotateSfx is not None:
-            rotateTrack.append(
-                Func(base.playSfx, self.rotateSfx, looping=1, node=self.avatar)
-            )
-        rotateTrack.append(self.getQuatInterval())
-        track.append(rotateTrack)
-
-        # Clean up what is necessary:
-        if self.rotateAnimName is not None:
-            track.append(
-                Func(self.avatar.setPlayRate, 1, self.rotateAnimName)
-            )
-        if self.rotateSfx is not None:
-            track.append(
-                Func(self.rotateSfx.stop)
-            )
-
-        # Then construct the walk track:
+        # Next, construct the walk track:
         walkTrack = Parallel()
-        if self.walkAnimName is not None:
+        if self.animName is not None:
             walkTrack.append(
-                Func(self.avatar.setPlayRate, self.walkAnimPlayRate,
-                     self.walkAnimName)
+                Func(self.avatar.setPlayRate, self.animPlayRate, self.animName)
             )
+            walkTrack.append(Func(self.avatar.loop, self.animName))
+        if self.sfx is not None:
             walkTrack.append(
-                Func(self.avatar.loop, self.walkAnimName)
-            )
-        if self.walkSfx is not None:
-            walkTrack.append(
-                SoundInterval(self.walkSfx, loop=1, node=self.avatar)
+                Func(base.playSfx, self.sfx, looping=1, node=self.avatar)
             )
         walkTrack.append(self.getPosInterval())
         track.append(walkTrack)
 
         # Finally, clean up what is necessary:
-        if self.walkAnimName is not None:
-            track.append(
-                Func(self.avatar.setPlayRate, 1, self.walkAnimName)
-            )
-        if self.walkSfx is not None:
-            track.append(
-                Func(self.walkSfx.stop)
-            )
+        if self.animName is not None:
+            track.append(Func(self.avatar.setPlayRate, 1, self.animName))
+        if self.sfx is not None:
+            track.append(Func(self.sfx.stop))
 
         return track
 
-    def getQuatInterval(self):
-        duration = abs((self.quatB.getHpr()-self.quatA.getHpr())[0]) / self.rotateSpeed
-        return LerpQuatInterval(
-            self.avatar, duration, quat=self.quatB, startQuat=self.quatA
-        )
-
     def getPosInterval(self):
-        duration = (self.pointB-self.pointA).length() / self.walkSpeed
+        duration = (self.pointB-self.pointA).length() / self.speed
         return LerpPosInterval(
             self.avatar, duration, pos=self.pointB, startPos=self.pointA
         )
