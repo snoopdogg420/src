4d80a9cbfd9bae1e3ccf7afaddc3b7e0b55abd42
xPythonic <gr33nmyth@aol.com>
2014-09-05 02:03:31 -0400

    DNA: Add the new and improved DNALoader

--- toontown/dna/DNALoader.py
+++ toontown/dna/DNALoader.py
@@ -1,218 +1,198 @@
-from pandac.PandaModules import *
-from direct.distributed.PyDatagramIterator import PyDatagramIterator
-from direct.distributed.PyDatagram import PyDatagram
-from direct.stdpy.file import *
-
-import DNAUtil
-import DNAError
-import DNAAnimBuilding
-import DNAAnimProp
-import DNACornice
-import DNADoor
-import DNAFlatBuilding
-import DNAFlatDoor
-import DNAGroup
-import DNAInteractiveProp
-import DNALandmarkBuilding
-import DNANode
-import DNAProp
-import DNASign
-import DNASignBaseline
-import DNASignGraphic
-import DNASignText
-import DNAStreet
-import DNAVisGroup
-import DNAWall
-import DNAWindows
-import DNABattleCell
-import DNASuitPoint
-
 import zlib
-import sys
-sys.setrecursionlimit(10000)
 
-compClassTable = {
-1: DNAGroup.DNAGroup,
-2: DNAVisGroup.DNAVisGroup,
-3: DNANode.DNANode,
-4: DNAProp.DNAProp,
-5: DNASign.DNASign,
-6: DNASignBaseline.DNASignBaseline,
-7: DNASignText.DNASignText,
-8: DNASignGraphic.DNASignGraphic,
-9: DNAFlatBuilding.DNAFlatBuilding,
-10: DNAWall.DNAWall,
-11: DNAWindows.DNAWindows,
-12: DNACornice.DNACornice,
-13: DNALandmarkBuilding.DNALandmarkBuilding,
-14: DNAAnimProp.DNAAnimProp,
-15: DNAInteractiveProp.DNAInteractiveProp,
-16: DNAAnimBuilding.DNAAnimBuilding,
-17: DNADoor.DNADoor,
-18: DNAFlatDoor.DNAFlatDoor,
-19: DNAStreet.DNAStreet
-}
+from toontown.dna import DNAAnimProp
+from toontown.dna import DNABattleCell
+from toontown.dna import DNACornice
+from toontown.dna import DNADoor
+from toontown.dna import DNAFlatBuilding
+from toontown.dna import DNAFlatDoor
+from toontown.dna import DNAGroup
+from toontown.dna import DNAInteractiveProp
+from toontown.dna import DNALandmarkBuilding
+from toontown.dna import DNAProp
+from toontown.dna import DNASign
+from toontown.dna import DNASignBaseline
+from toontown.dna import DNASignGraphic
+from toontown.dna import DNASignText
+from toontown.dna import DNAStreet
+from toontown.dna import DNASuitPoint
+from toontown.dna import DNAVisGroup
+from toontown.dna import DNAWall
+from toontown.dna import DNAWindows
+from toontown.dna.DNAPacker import *
 
-childlessComps = (
-7, # DNASignText
-11, # DNAWindows
-12, # DNACornice
-17, # DNADoor
-18, # DNAFlatDoor
-19 # DNAStreet
-)
+
+class DNAError(Exception):
+    pass
+
+
+# We'll want to access DNAError in other parts of the application:
+__builtins__['DNAError'] = DNAError
+
 
 class DNALoader:
     def __init__(self):
-        self.dnaStorage = None
-        self.prop = None
+        self.storage = None
+        self.packer = DNAPacker()
+        self.topGroup = None
 
     def destroy(self):
-        del self.dnaStorage
-        del self.prop
+        self.storage = None
+        self.packer = None
+        self.topGroup = None
 
-    def handleStorageData(self, dgi):
-        # Catalog Codes
-        numRoots = dgi.getUint16()
-        for _ in xrange(numRoots):
-            root = DNAUtil.dgiExtractString8(dgi)
-            numCodes = dgi.getUint8()
-            for i in xrange(numCodes):
-                code = DNAUtil.dgiExtractString8(dgi)
-                self.dnaStorage.storeCatalogCode(root, code)
+    def load(self, storage, filepath):
+        self.storage = storage
 
-        # Textures
-        numTextures = dgi.getUint16()
-        for _ in xrange(numTextures):
-            code = DNAUtil.dgiExtractString8(dgi)
-            filename = DNAUtil.dgiExtractString8(dgi)
-            self.dnaStorage.storeTexture(code, loader.pdnaTexture(filename, okMissing=True))
+        # Load the PDNA file and parse the header:
+        with open(filepath, 'rb') as f:
+            data = f.read()
+            if not data.startswith('PDNA\n'):
+                raise DNAError('Invalid libpandadna header.')
+            data = data[5:]
 
-        # Fonts
-        numFonts = dgi.getUint16()
-        for _ in xrange(numFonts):
-            code = DNAUtil.dgiExtractString8(dgi)
-            filename = DNAUtil.dgiExtractString8(dgi)
-            self.dnaStorage.storeFont(code, loader.pdnaFont(filename))
+            # Is this PDNA file compressed? If it is, decompress it using ZLib:
+            compressed = int(ord(data[0]))
+            data = data[2:]
+            if compressed:
+                zlib.decompress(data)
 
-        # Nodes
-        self.handleNode(dgi, target = self.dnaStorage.storeNode)
-        self.handleNode(dgi, target = self.dnaStorage.storeHoodNode)
-        self.handleNode(dgi, target = self.dnaStorage.storePlaceNode)
+        # Add the data to the DNAPacker:
+        self.packer += data
 
-        # Blocks
-        numBlocks = dgi.getUint16()
-        for _ in xrange(numBlocks):
-            number = dgi.getUint8()
-            zone = dgi.getUint16()
-            title = DNAUtil.dgiExtractString8(dgi)
-            article = DNAUtil.dgiExtractString8(dgi)
-            bldgType = DNAUtil.dgiExtractString8(dgi)
-            self.dnaStorage.storeBlock(number, title, article, bldgType, zone)
+        # Construct the storage and components:
+        self.readStorage()
+        self.readComponents()
 
-        # Suit Points
-        numPoints = dgi.getUint16()
-        for _ in xrange(numPoints):
-            index = dgi.getUint16()
-            pointType = dgi.getUint8()
-            x, y, z = (dgi.getInt32() / 100.0 for i in xrange(3))
-            graph = dgi.getUint8()
-            landmarkBuildingIndex = dgi.getInt8()
-            self.dnaStorage.storeSuitPoint(DNASuitPoint.DNASuitPoint(index, pointType, LVector3f(x, y, z), landmarkBuildingIndex))
+    def readStorage(self):
+        if self.storage is None:
+            raise DNAError('Tried to read storage before one was present.')
 
-        # Suit Edges
-        numEdges = dgi.getUint16()
-        for _ in xrange(numEdges):
-            index = dgi.getUint16()
-            numPoints = dgi.getUint16()
-            for i in xrange(numPoints):
-                endPoint = dgi.getUint16()
-                zoneId = dgi.getUint16()
-                self.dnaStorage.storeSuitEdge(index, endPoint, zoneId)
+        # Catalog codes...
+        rootCount = self.packer.unpack(UINT16)
+        for _ in xrange(rootCount):
+            root = self.packer.unpack(SHORT_STRING)
+            codeCount = self.packer.unpack(UINT8)
+            for _ in xrange(codeCount):
+                code = self.packer.unpack(SHORT_STRING)
+                self.storage.storeCatalogCode(root, code)
 
-        # Battle Cells
-        numCells = dgi.getUint16()
-        for _ in xrange(numCells):
-            w = dgi.getUint8()
-            h = dgi.getUint8()
-            x, y, z = (dgi.getInt32() / 100.0 for i in xrange(3))
-            self.dnaStorage.storeBattleCell(DNABattleCell.DNABattleCell(w, h, LVector3f(x, y, z)))
+        # Textures...
+        textureCount = self.packer.unpack(UINT16)
+        for _ in xrange(textureCount):
+            code = self.packer.unpack(SHORT_STRING)
+            filename = self.packer.unpack(SHORT_STRING)
+            self.storage.storeTexture(code, filename)
 
-    def handleCompData(self, dgi):
-        propCode = dgi.getUint8()
-        if propCode == 255:
-            if self.prop == None:
-                raise DNAError.DNAError('Unexpected 255 found.')
-            prop = self.prop.getParent()
-            if prop is not None:
-                self.prop = prop
-            else:
-                assert self.prop.getName() == 'root'
-        elif propCode in compClassTable:
-            propClass = compClassTable[propCode]
-            if propClass.__init__.func_code.co_argcount > 1:
-                newComp = propClass('unnamed_comp')
-            else:
-                newComp = propClass()
-            if propCode == 2:
-                newComp.makeFromDGI(dgi, self.dnaStorage)
-                self.dnaStorage.storeDNAVisGroup(newComp)
-            else:
-                newComp.makeFromDGI(dgi)
+        # Fonts...
+        fontCount = self.packer.unpack(UINT16)
+        for _ in xrange(fontCount):
+            code = self.packer.unpack(SHORT_STRING)
+            filename = self.packer.unpack(SHORT_STRING)
+            self.storage.storeFont(code, filename)
+
+        # Nodes...
+        nodeCount = self.packer.unpack(UINT16)
+        for _ in xrange(nodeCount):
+            code = self.packer.unpack(SHORT_STRING)
+            filename = self.packer.unpack(SHORT_STRING)
+            search = self.packer.unpack(SHORT_STRING)
+            self.storage.storeNode(code, filename, search)
+
+        # Blocks...
+        blockNumberCount = self.packer.unpack(UINT16)
+        for _ in xrange(blockNumberCount):
+            number = self.packer.unpack(UINT8)
+            zoneId = self.packer.unpack(UINT16)
+            title = self.packer.unpack(SHORT_STRING)
+            article = self.packer.unpack(SHORT_STRING)
+            buildingType = self.packer.unpack(SHORT_STRING)
+            self.storage.storeBlockNumber(number)
+            self.storage.storeBlockZone(number, zoneId)
+            if title:
+                self.storage.storeBlockTitle(number, title)
+            if article:
+                self.storage.storeBlockArticle(number, article)
+            if buildingType:
+                self.storage.storeBlockBuildingType(number, buildingType)
+
+        # Suit points...
+        suitPointCount = self.packer.unpack(UINT16)
+        for _ in xrange(suitPointCount):
+            index = self.packer.unpack(UINT16)
+            pointType = self.packer.unpack(UINT8)
+            pos = self.packer.unpackPosition()
+            landmarkBuildingIndex = self.packer.unpack(INT8)
+            suitPoint = DNASuitPoint.DNASuitPoint(
+                index, pointType, pos,
+                landmarkBuildingIndex=landmarkBuildingIndex)
+            self.storage.storeSuitPoint(suitPoint)
+
+        # Suit edges...
+        suitEdgeCount = self.packer.unpack(UINT16)
+        for _ in xrange(suitEdgeCount):
+            startPointIndex = self.packer.unpack(UINT16)
+            edgeCount = self.packer.unpack(UINT16)
+            for _ in xrange(edgeCount):
+                endPointIndex = self.packer.unpack(UINT16)
+                zoneId = self.packer.unpack(UINT16)
+                self.storage.storeSuitEdge(startPointIndex, endPointIndex, zoneId)
+
+        # Battle cells...
+        battleCellCount = self.packer.unpack(UINT16)
+        for _ in xrange(battleCellCount):
+            width = self.packer.unpack(UINT8)
+            height = self.packer.unpack(UINT8)
+            pos = self.packer.unpackPosition()
+            cell = DNABattleCell.DNABattleCell(width, height, pos)
+            self.storage.storeBattleCell(cell)
+
+    def readComponent(self, ctor):
+        component = ctor('')
+        hasChildren = component.construct(self.dnaStore, self.packer)
+        if self.topGroup is not None:
+            self.topGroup.add(component)
+            component.setParent(self.topGroup)
+        if hasChildren:
+            self.topGroup = component
+
+    def readComponents(self):
+        componentCode = self.packer.unpack(UINT8)
+        if componentCode == DNAGroup.DNAGroup.COMPONENT_CODE:
+            self.readComponent(DNAGroup.DNAGroup)
+        elif componentCode == DNAVisGroup.DNAVisGroup.COMPONENT_CODE:
+            self.readComponent(DNAVisGroup.DNAVisGroup)
+        elif componentCode == DNAProp.DNAProp.COMPONENT_CODE:
+            self.readComponent(DNAProp.DNAProp)
+        elif componentCode == DNASign.DNASign.COMPONENT_CODE:
+            self.readComponent(DNASign.DNASign)
+        elif componentCode == DNASignBaseline.DNASignBaseline.COMPONENT_CODE:
+            self.readComponent(DNASignBaseline.DNASignBaseline)
+        elif componentCode == DNASignText.DNASignText.COMPONENT_CODE:
+            self.readComponent(DNASignText.DNASignText)
+        elif componentCode == DNASignGraphic.DNASignGraphic.COMPONENT_CODE:
+            self.readComponent(DNASignGraphic.DNASignGraphic)
+        elif componentCode == DNAFlatBuilding.DNAFlatBuilding.COMPONENT_CODE:
+            self.readComponent(DNAFlatBuilding.DNAFlatBuilding)
+        elif componentCode == DNAWall.DNAWall.COMPONENT_CODE:
+            self.readComponent(DNAWall.DNAWall)
+        elif componentCode == DNAWindows.DNAWindows.COMPONENT_CODE:
+            self.readComponent(DNAWindows.DNAWindows)
+        elif componentCode == DNACornice.DNACornice.COMPONENT_CODE:
+            self.readComponent(DNACornice.DNACornice)
+        elif componentCode == DNALandmarkBuilding.DNALandmarkBuilding.COMPONENT_CODE:
+            self.readComponent(DNALandmarkBuilding.DNALandmarkBuilding)
+        elif componentCode == DNAAnimProp.DNAAnimProp.COMPONENT_CODE:
+            self.readComponent(DNAAnimProp.DNAAnimProp)
+        elif componentCode == DNAInteractiveProp.DNAInteractiveProp.COMPONENT_CODE:
+            self.readComponent(DNAInteractiveProp.DNAInteractiveProp)
+        elif componentCode == DNADoor.DNADoor.COMPONENT_CODE:
+            self.readComponent(DNADoor.DNADoor)
+        elif componentCode == DNAFlatDoor.DNAFlatDoor.COMPONENT_CODE:
+            self.readComponent(DNAFlatDoor.DNAFlatDoor)
+        elif componentCode == DNAStreet.DNAStreet.COMPONENT_CODE:
+            self.readComponent(DNAStreet.DNAStreet)
         else:
-            raise DNAError.DNAError('Invalid prop code: %d' % propCode)
-        if dgi.getRemainingSize():
-            if propCode != 255:
-                if self.prop is not None:
-                    newComp.setParent(self.prop)
-                    self.prop.add(newComp)
-                if propCode not in childlessComps:
-                    self.prop = newComp
-            self.handleCompData(dgi)
-
-    def handleNode(self, dgi, target = None):
-        if target is None:
-            return
-        numNodes = dgi.getUint16()
-        for _ in xrange(numNodes):
-            code = DNAUtil.dgiExtractString8(dgi)
-            file = DNAUtil.dgiExtractString8(dgi)
-            node = DNAUtil.dgiExtractString8(dgi)
-            np = NodePath(loader.pdnaModel(file))
-            if node:
-                newNode = np.find('**/' + node).copyTo(NodePath())
-                np.removeNode()
-                np = newNode
-            np.setTag('DNACode', code)
-            np.setTag('DNARoot', node)
-            target(np, code)
-
-    def loadDNAFileBase(self, dnaStorage, file):
-        self.dnaStorage = dnaStorage
-        dnaFile = open(file, 'rb')
-        dnaData = dnaFile.read()
-        dg = PyDatagram(dnaData)
-        dgi = PyDatagramIterator(dg)
-        dnaFile.close()
-        header = dgi.extractBytes(5)
-        if header != 'PDNA\n':
-            raise DNAError.DNAError('Invalid header: %s' % (header))
-        compressed = dgi.getBool()
-        dgi.skipBytes(1)
-        if compressed:
-            data = dgi.getRemainingBytes()
-            data = zlib.decompress(data)
-            dg = PyDatagram(data)
-            dgi = PyDatagramIterator(dg)
-        self.handleStorageData(dgi)
-        self.handleCompData(dgi)
-
-    def loadDNAFile(self, dnaStorage, file):
-        self.loadDNAFileBase(dnaStorage, file)
-        nodePath = NodePath(PandaNode('dna'))
-        self.prop.traverse(nodePath, self.dnaStorage)
-        return nodePath
-
-    def loadDNAFileAI(self, dnaStorage, file):
-        self.loadDNAFileBase(dnaStorage, file)
-        return self.prop
+            self.topGroup = self.topGroup.getParent()
+        if self.packer:
+            self.readComponents()
