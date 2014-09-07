de05eeccc52f812233959f77cec886aeb0da6758
xPythonic <gr33nmyth@aol.com>
2014-09-07 10:49:22 -0400

    Nametag: Fix NametagFloat2d
    I give up on trying to do fancy stuff with this...

--- toontown/nametag/NametagFloat2d.py
+++ toontown/nametag/NametagFloat2d.py
@@ -1,4 +1,5 @@
-from toontown.nametag.Nametag2d import Nametag2d
+from pandac.PandaModules import Point3, Point2
+
 from toontown.nametag.NametagFloat3d import NametagFloat3d
 
 
@@ -15,4 +16,22 @@ class NametagFloat2d(NametagFloat3d):
             self.region.setActive(False)
 
     def setClickRegion(self, left, right, bottom, top):
-        Nametag2d.setClickRegion(self, left, right, bottom, top)
+        # Get a transform matrix to position the points correctly according to
+        # the nametag node:
+        transform = self.contents.getNetTransform()
+
+        # Get the actual matrix of the transform above:
+        mat = transform.getMat()
+
+        # Transform the specified points to the new matrix:
+        camSpaceTopLeft = mat.xformPoint(Point3(left, 0, top))
+        camSpaceBottomRight = mat.xformPoint(Point3(right, 0, bottom))
+
+        screenSpaceTopLeft = Point2(camSpaceTopLeft[0], camSpaceTopLeft[2])
+        screenSpaceBottomRight = Point2(camSpaceBottomRight[0], camSpaceBottomRight[2])
+
+        left, top = screenSpaceTopLeft
+        right, bottom = screenSpaceBottomRight
+
+        self.region.setFrame(left, right, bottom, top)
+        self.region.setActive(True)
