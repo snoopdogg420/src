f42c709f6d85e3fedaa68682dde67517907c2c8f
Abrupt <unaffiliate@outlook.com>
2014-09-15 16:31:01 -0400

    OTPClientRepository: Removed parentheses  on elif self.bootedIndex == 155.

--- otp/distributed/OTPClientRepository.py
+++ otp/distributed/OTPClientRepository.py
@@ -912,7 +912,7 @@ class OTPClientRepository(ClientRepositoryBase):
         self.stopReaderPollTask()
         if (self.bootedIndex is not None) and (self.bootedIndex in OTPLocalizer.CRBootedReasons):
             message = OTPLocalizer.CRBootedReasons[self.bootedIndex]
-        elif(self.bootedIndex == 155):
+        elif self.bootedIndex == 155:
             message = self.bootedText
         elif self.bootedText is not None:
             message = OTPLocalizer.CRBootedReasonUnknownCode % self.bootedIndex
