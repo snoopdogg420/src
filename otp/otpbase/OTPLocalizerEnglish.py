71b2df194f2dcbbd51673b96257a66d4e4a02ec4
Abrupt <unaffiliate@outlook.com>
2014-09-16 22:44:47 -0400

    OTPLocalizerEnglish: Enhanced the maintenance message (disconnect code 154).

--- otp/otpbase/OTPLocalizerEnglish.py
+++ otp/otpbase/OTPLocalizerEnglish.py
@@ -190,8 +190,8 @@ CRBootedReasons = {1: 'An unexpected problem has occurred.  Your connection has 
  151: 'You have been logged out by an administrator working on the servers.',
  152: "There has been a reported violation of our Terms of Use connected to '%(name)s'. For more details, please review the message sent to the e-mail address associated with '%(name)s'.",
  153: 'The district you were playing on has been reset.  Everyone who was playing on that district has been disconnected.  However, you should be able to connect again and go right back into the game.',
- 154: 'The game is temporarily closing for maintenance.  Everyone has been logged out.',
- 155: 'You were kicked by a Moderator. Behave next time!',
+ 154: 'Toontown is now closed for maintenance. Everyone has been disconnected from the game.\n\nFor more information, please visit https://toontowninfinite.com.',
+ 155: 'You were kicked by a moderator. Behave next time!',
  288: 'Sorry, you have used up all of your available minutes this month.',
  349: 'Sorry, you have used up all of your available minutes this month.'}
 CRBootedReasonUnknownCode = 'An unexpected problem has occurred (error code %s).  Your connection has been lost, but you should be able to connect again and go right back into the game.'
