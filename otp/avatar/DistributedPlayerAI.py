5e612c69e952f30d83fc39508f1660c5c9566d9a
Abrupt <unaffiliate@outlook.com>
2014-09-16 23:22:55 -0400

    DistributedPlayerAI: The bootedText now includes infinite.

--- otp/avatar/DistributedPlayerAI.py
+++ otp/avatar/DistributedPlayerAI.py
@@ -173,7 +173,7 @@ def maintenance(minutes):
         dg = PyDatagram()
         dg.addServerHeader(10, simbase.air.ourChannel, CLIENTAGENT_EJECT)
         dg.addUint16(154)
-        dg.addString('Toontown is now closed for maintenance.')
+        dg.addString('Toontown Infinite is now closed for maintenance.')
         simbase.air.send(dg)
         return Task.done
     
