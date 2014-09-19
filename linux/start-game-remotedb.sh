3c902a83101caf2cef1dbf0aed0a10e6f4757b8a
Kevin <kvn_hammett@yahoo.com>
2014-09-18 22:19:45 -0400

    Update Linux Start

--- linux/start-game-remotedb.sh
+++ linux/start-game-remotedb.sh
@@ -5,8 +5,8 @@ cd ..
 read -p "Username: " ttiUsername
 read -s -p "Password: " ttiPassword
 echo
-read -p "Gameserver (DEFAULT: 192.99.200.107): " TTI_GAMESERVER
-TTI_GAMESERVER=${TTI_GAMESERVER:-"192.99.200.107"}
+read -p "Gameserver (DEFAULT: 167.114.28.238): " TTI_GAMESERVER
+TTI_GAMESERVER=${TTI_GAMESERVER:-"167.114.28.238"}
 
 # Export the environment variables:
 export ttiUsername=$ttiUsername
