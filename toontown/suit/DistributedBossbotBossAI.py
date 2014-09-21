375a40c078087ed0a5623d56d6901e1627684bbd
Chan <chandler14362@gmail.com>
2014-09-21 11:21:38 -0400

    Added a hack check to hitBoss in DistributedBossbotBossAI

--- toontown/suit/DistributedBossbotBossAI.py
+++ toontown/suit/DistributedBossbotBossAI.py
@@ -463,11 +463,18 @@ class DistributedBossbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
 
         self.battleFourSetup = True
 
+    def hitBossHackCheck(self, avId, bossDamage):
+        if bossDamage > 3: # Hacker
+            # Ban them.
+            self.air.banManager.ban(avId, "hacking", 0)
+            return 0
+        return bossDamage
+
     def hitBoss(self, bossDamage):
         avId = self.air.getAvatarIdFromSender()
         if not self.validate(avId, avId in self.involvedToons, 'hitBoss from unknown avatar'):
             return
-        self.validate(avId, bossDamage <= 3, 'invalid bossDamage %s' % bossDamage)
+        bossDamage = self.hitBossHackCheck(avId, bossDamage)
         if bossDamage < 1:
             return
         currState = self.getCurrentOrNextState()
