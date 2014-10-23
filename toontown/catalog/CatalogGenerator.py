54bfe1e19c015d5f42881809b0492f094d20d92d
chandler14362 <chandler14362@gmail.com>
2014-10-23 08:54:29 -0400

    Estates: Disabled the Gardening Kit and 24 Hours of Cannons in the CatalogGenerator
    They will be re-enabled once I start to work on the code for them.

--- toontown/catalog/CatalogGenerator.py
+++ toontown/catalog/CatalogGenerator.py
@@ -511,8 +511,8 @@ MonthlySchedule = ((7,
    CatalogGardenItem(103, 1),
    CatalogGardenItem(104, 1),
    CatalogToonStatueItem(105, endPoseIndex=108),
-   CatalogRentalItem(1, 2880, 1000),
-   CatalogGardenStarterItem(),
+   #CatalogRentalItem(1, 2880, 1000), # TODO
+   #CatalogGardenStarterItem(), # TODO
    CatalogNametagItem(100),
    CatalogNametagItem(0),
    CatalogClothingItem(1608, 0, 0),
