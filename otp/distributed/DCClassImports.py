cbb8453c384c5f1edfde72a3d10022cae38d06ee
Chan <chandler14362@gmail.com>
2014-10-24 23:25:57 -0400

    Parsed DCClassImports

--- otp/distributed/DCClassImports.py
+++ otp/distributed/DCClassImports.py
@@ -2,7 +2,7 @@
 from pandac.PandaModules import *
 
 
-hashVal = 2422226345L
+hashVal = 242241157
 
 
 from toontown.cogdominium import DistributedCogdoInterior, DistributedCogdoBattleBldg, DistributedCogdoElevatorExt, DistributedCogdoElevatorInt, DistributedCogdoBarrel, DistCogdoGame, DistCogdoLevelGame, DistCogdoBoardroomGame, DistCogdoCraneGame, DistCogdoMazeGame, DistCogdoFlyingGame, DistCogdoCrane, DistCogdoCraneMoneyBag, DistCogdoCraneCog
