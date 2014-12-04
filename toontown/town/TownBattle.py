4c3b48522506eb0155bf9f426e89087c76c87f46
Chan <chandler14362@gmail.com>
2014-12-04 14:58:40 -0500

    Fixed a bug that caused the laff meter in battles to show the incorrect laff

--- toontown/town/TownBattle.py
+++ toontown/town/TownBattle.py
@@ -458,7 +458,15 @@ class TownBattle(StateData.StateData):
         if resetActivateMode:
             self.__enterPanels(self.numToons, self.localNum)
             for i in xrange(len(toons)):
-                self.toonPanels[i].setLaffMeter(toons[i])
+
+                # For the laff meter we want to check if we have an experiment toon
+                toon = toons[i]
+                if toon.doId != base.localAvatar.altDoId: # This means we arent dealing with our local toon
+                    if toon.altDoId: # Check if they have an altDoId
+                        if toon.altDoId != toon.doId: # Check if they have more then one toon
+                            toon = base.cr.doId2do[toon.altDoId] # Use their experiment toon
+
+                self.toonPanels[i].setLaffMeter(toon)
 
             if currStateName == 'ChooseCog':
                 self.chooseCogPanel.adjustCogs(self.numCogs, self.luredIndices, self.trappedIndices, self.track)
