a024f4f97a84b15e5ccaa7bc20456028348b0e9b
Josh Siegel <jsiegel@stormbirds.org>
2014-12-05 14:03:39 -0600

    prevent the ai from crashing.. How did we get here?

--- toontown/catalog/CatalogFurnitureItem.py
+++ toontown/catalog/CatalogFurnitureItem.py
@@ -1104,7 +1104,10 @@ class CatalogFurnitureItem(CatalogAtticItem.CatalogAtticItem):
         dg.addInt16(self.furnitureType)
         if FurnitureTypes[self.furnitureType][FTColorOptions]:
             if store & CatalogItem.Customization:
-                dg.addUint8(self.colorOption)
+                if self.colorOption == None:
+                    dg.addUint8(0)
+                else:
+                    dg.addUint8(self.colorOption)
 
     def getAcceptItemErrorText(self, retcode):
         if retcode == ToontownGlobals.P_AlreadyOwnBiggerCloset:
