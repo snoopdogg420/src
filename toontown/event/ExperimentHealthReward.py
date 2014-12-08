08aa6b5a10ab85a94096c8bdb54c4a1de03dbbc1
Chan <chandler14362@gmail.com>
2014-12-07 20:34:08 -0500

    Added ExperimentHealthReward

--- toontown/event/ExperimentHealthReward.py
+++ toontown/event/ExperimentHealthReward.py
@@ -2,4 +2,19 @@ from toontown.event.ExperimentReward import ExperimentReward
 
 
 class ExperimentHealthReward(ExperimentReward):
-    pass
+    def __init__(self, challenge, hpAmount):
+        ExperimentReward.__init__(self, challenge)
+
+        self.hpAmount = hpAmount
+
+    def giveReward(self):
+        for avId in self.participants:
+            av = self.air.doId2do.get(avId)
+            if av is None:
+                continue
+
+            av.b_setMaxHp(av.getMaxHp() + self.hpAmount)
+            av.toonUp(av.getMaxHp())
+
+    def notifyReward(self):
+        self.messageParticipants('You have earned a laff boost!')
