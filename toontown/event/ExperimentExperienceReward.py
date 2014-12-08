7065fa3402492e9d1bc9630107cfe4aef73d567d
Chan <chandler14362@gmail.com>
2014-12-07 20:40:22 -0500

    Added ExperimentExperienceReward

--- toontown/event/ExperimentExperienceReward.py
+++ toontown/event/ExperimentExperienceReward.py
@@ -1,5 +1,21 @@
 from toontown.event.ExperimentReward import ExperimentReward
 
+import random
+
 
 class ExperimentExperienceReward(ExperimentReward):
-    pass
+    def giveReward(self):
+        for avId in self.participants:
+            av = self.air.doId2do.get(avId)
+            if av is None:
+                continue
+
+            for track, hasAccess in enumerate(av.trackArray):
+                if hasAccess:
+                    xp = av.experience.getNextExpValue(track) * random.random()
+                    av.experience.setExp(xp)
+
+            av.d_setExperience(av.experience.makeNetString())
+
+    def notifyReward(self):
+        self.messageParticipants('You have earned some extra Gag points!')
