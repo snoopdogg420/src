75689590f6de69f885fc9287c5405798e446600e
Chan <chandler14362@gmail.com>
2014-12-11 22:42:05 -0500

    Fade the logo in during the credits

--- toontown/event/ExperimentCredits.py
+++ toontown/event/ExperimentCredits.py
@@ -1,6 +1,7 @@
-from pandac.PandaModules import CardMaker
-from direct.interval.IntervalGlobal import Sequence, Func
+from pandac.PandaModules import CardMaker, TransparencyAttrib
+from direct.interval.IntervalGlobal import Sequence, Func, Wait
 from direct.interval.LerpInterval import LerpColorScaleInterval
+from direct.gui.OnscreenImage import OnscreenImage
 
 
 class ExperimentCredits:
@@ -9,27 +10,65 @@ class ExperimentCredits:
 
         cm = CardMaker('screen-cover')
         cm.setFrameFullscreenQuad()
-        self.screenCover = aspect2d.attachNewNode(cm.generate())
+        self.screenCover = render2d.attachNewNode(cm.generate())
         self.screenCover.hide()
-        self.screenCover.setScale(10)
+        self.screenCover.setScale(100)
         self.screenCover.setColor((0, 0, 0, 1))
 
-    def getScreenFadeBlack(self):
-        return Sequence(Func(self.screenCover.show),
-                        Func(self.screenCover.setTransparency, 1),
-                        LerpColorScaleInterval(self.screenCover, 3, Vec4(1, 1, 1, 1),
-                                               startColorScale=Vec4(1, 1, 1, 0)),
-                        Func(self.screenCover.clearColorScale),
-                        Func(self.screenCover.clearTransparency))
+        self.logo = OnscreenImage(image='phase_3/maps/toontown-logo.png',
+                                  scale=(1.0 * (4.0/3.0), 1, 1.0 / (4.0/3.0)),
+                                  pos=(-0.05, 1, -0.85))
+        self.logo.setTransparency(TransparencyAttrib.MAlpha)
+        self.logo.reparentTo(base.a2dTopCenter)
+        self.logo.hide()
+
+        self.creditsSeq = None
+
+    def disableToonInterface(self):
+        pass
+
+    def enableToonInterface(self):
+        pass
 
     def start(self):
         base.musicManager.stopAllSounds()
 
+        self.disableToonInterface()
+
         self.music.play()
-        Sequence(self.getScreenFadeBlack()).start()
+
+
+        self.creditsSeq = Sequence(
+
+        # Fade the screen black
+        Sequence(Func(self.screenCover.show),
+                        Func(self.screenCover.setTransparency, 1),
+                        LerpColorScaleInterval(self.screenCover, 4, Vec4(1, 1, 1, 1),
+                                               startColorScale=Vec4(1, 1, 1, 0)),
+                        Func(self.screenCover.clearColorScale),
+                        Func(self.screenCover.clearTransparency)),
+
+        # Fade the logo in
+        Sequence(Func(self.logo.show),
+                 LerpColorScaleInterval(self.logo, 2, Vec4(1, 1, 1, 1),
+                                        startColorScale=Vec4(1, 1, 1, 0)),
+                 Func(self.logo.clearColorScale)),
+
+        # Wait 2 seconds
+        Wait(2.0),
+
+        # Fade the logo out
+        Sequence(LerpColorScaleInterval(self.logo, 5, Vec4(1, 1, 1, 0),
+                                        startColorScale=Vec4(1, 1, 1, 1)),
+                 Func(self.logo.clearColorScale),
+                 Func(self.logo.hide)),
+
+        )
+
+        self.creditsSeq.start()
 
     def stop(self):
-        self.screenCover.hide()
+        pass
 
     def cleanup(self):
-        self.screenCover.removeNode()
+        pass
