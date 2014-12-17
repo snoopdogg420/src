0e0a65b3dcfee0bc07d22b1c3cd5f68a67aec955
Chan <chandler14362@gmail.com>
2014-12-17 18:54:19 -0500

    CraneGame updates

--- toontown/cogdominium/DistCogdoCraneGameAI.py
+++ toontown/cogdominium/DistCogdoCraneGameAI.py
@@ -1,9 +1,16 @@
 from toontown.cogdominium.DistCogdoLevelGameAI import DistCogdoLevelGameAI
 from toontown.cogdominium import CogdoCraneGameSpec
+from otp.level.LevelSpec import LevelSpec
 
 
 class DistCogdoCraneGameAI(DistCogdoLevelGameAI):
     notify = directNotify.newCategory('DistCogdoCraneGameAI')
 
     def getLevelSpec(self):
-        return CogdoCraneGameSpec.levelSpec
+        return LevelSpec(CogdoCraneGameSpec)
+
+    def startHandleEdits(self):
+        pass
+
+    def stopHandleEdits(self):
+        pass
